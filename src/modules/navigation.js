faca16c5f8f3c895abea2d7028dec1e1e587864d
Chris-Wilco <christopher.wilcoski@gmail.com>
2024-03-26 15:52:54 -0700

    clean up refactored nav generation

--- src/modules/navigation.js
+++ src/modules/navigation.js
@@ -1,52 +1,7 @@
-/* import * as ProjectNavVisual from "./projectNavVisual.js";
-import * as GenerateElement from "./generatePageElement.js"; */
-
 import * as NavigationVisual from "./navigationVisual.js";
 
 export function create(projectList) {
     const navVisual = NavigationVisual.create(projectList);
-
-    /* const navContainer = GenerateElement.generatePageElement("div", [
-        "nav-container",
-    ]);
-
-    const navTitleContainer = GenerateElement.generatePageElement(
-        "div",
-        ["nav-title-container"],
-        navContainer
-    );
-
-    const navTitle = GenerateElement.generatePageElement(
-        "div",
-        ["nav-title"],
-        navTitleContainer,
-        "My Projects"
-    );
-
-    const addNewProjectButton = GenerateElement.generatePageElement(
-        "div",
-        ["new-project-button"],
-        navTitleContainer
-    );
-
-    const navContentContainer = GenerateElement.generatePageElement(
-        "div",
-        ["nav-content-container"],
-        navContainer
-    );
-
-    projectList.forEach((project) => {
-        const navProjectVisual = ProjectNavVisual.create(
-            project.title,
-            project.lists
-        );
-
-        navContentContainer.appendChild(navProjectVisual);
-    });
-
-    navContainer.appendChild(navContentContainer);
-
-    return navContainer; */
 
     return { navVisual };
 }
--- src/modules/navigationVisual.js
+++ src/modules/navigationVisual.js
@@ -1,5 +1,3 @@
-/* import * as GenerateElement from "./generatePageElement.js";
- */
 import * as ProjectNavVisual from "./projectNavVisual.js";
 import * as GenerateElement from "./generatePageElement.js";
 
@@ -46,41 +44,3 @@ export function create(projectList) {
 
     return navContainer;
 }
-
-/* export function create(project) {
-    const projectContainer = GenerateElement.generatePageElement("div", [
-        "nav-project-container",
-    ]);
-
-    const projectTitleContainer = GenerateElement.generatePageElement(
-        "div",
-        ["nav-project-title-container"],
-        projectContainer
-    );
-
-    const projectTitleIcon = GenerateElement.generatePageElement(
-        "div",
-        ["nav-project-title-icon"],
-        projectTitleContainer
-    );
-
-    const projectTitle = GenerateElement.generatePageElement(
-        "div",
-        ["nav-project-title"],
-        projectTitleContainer,
-        project.title
-    );
-
-    const projectContentContainer = GenerateElement.generatePageElement(
-        "div",
-        ["nav-project-content-container"],
-        projectContainer
-    );
-
-    project.lists.forEach((list) => {
-        projectContentContainer.appendChild(list.listNavVisual);
-    });
-
-    return projectContainer;
-}
- */
